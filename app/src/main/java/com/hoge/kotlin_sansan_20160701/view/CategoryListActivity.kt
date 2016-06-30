fa390e638b07093ba5a28aebdef5fedca5df81f5
jumperson <kazuya.fb@gmail.com>
2016-07-01 08:51:22 +0900

    Activity名変更時のlayoutxmlの名前変更漏れ対応。

--- app/src/main/java/com/hoge/kotlin_sansan_20160701/view/CategoryListActivity.kt
+++ app/src/main/java/com/hoge/kotlin_sansan_20160701/view/CategoryListActivity.kt
@@ -8,6 +8,6 @@ class CategoryListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_category_list)
     }
 }
