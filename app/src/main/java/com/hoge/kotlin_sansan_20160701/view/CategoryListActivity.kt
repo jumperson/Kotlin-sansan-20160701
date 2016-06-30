dc05de9259bdeaa0da6b1d5e1ea121cad22f00f1
jumperson <kazuya.fb@gmail.com>
2016-07-01 08:30:49 +0900

    パッケージわけと初回起動のActivity名を変更。

--- app/src/main/java/com/hoge/kotlin_sansan_20160701/view/CategoryListActivity.kt
+++ app/src/main/java/com/hoge/kotlin_sansan_20160701/view/CategoryListActivity.kt
@@ -1,9 +1,10 @@
-package com.hoge.kotlin_sansan_20160701
+package com.hoge.kotlin_sansan_20160701.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.hoge.kotlin_sansan_20160701.R
 
-class MainActivity : AppCompatActivity() {
+class CategoryListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
