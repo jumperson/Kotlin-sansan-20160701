5e523a5cc4ad47a51a48a903d7af2ea827d3d948
jumperson <kazuya.fb@gmail.com>
2016-07-01 09:38:58 +0900

    RecyclerViewでデータ表示。

--- app/src/main/java/com/hoge/kotlin_sansan_20160701/view/CategoryListActivity.kt
+++ app/src/main/java/com/hoge/kotlin_sansan_20160701/view/CategoryListActivity.kt
@@ -2,12 +2,21 @@ package com.hoge.kotlin_sansan_20160701.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
 import com.hoge.kotlin_sansan_20160701.R
+import com.hoge.kotlin_sansan_20160701.view.CategoryListRecyclerViewAdapter
+import com.hoge.kotlin_sansan_20160701.viewmodel.CategoryListViewModel
 
 class CategoryListActivity : AppCompatActivity() {
+
+    private val viewModel = CategoryListViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category_list)
+
+        val r = findViewById(R.id.recyclerView) as RecyclerView
+
+        r.adapter = CategoryListRecyclerViewAdapter(viewModel.listItems)
     }
 }
---
+++ app/src/main/java/com/hoge/kotlin_sansan_20160701/view/CategoryListRecyclerViewAdapter.kt
@@ -0,0 +1,74 @@
+package com.hoge.kotlin_sansan_20160701.view
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.hoge.kotlin_sansan_20160701.databinding.CaptionItemBinding
+import com.hoge.kotlin_sansan_20160701.databinding.CategoryItemBinding
+import com.hoge.kotlin_sansan_20160701.databinding.SubCaptionItemBinding
+import com.hoge.kotlin_sansan_20160701.viewmodel.CategoryListViewModel
+
+
+/**
+ * Created by okamoto_kazuya on 16/07/01.
+ */
+
+class CategoryListRecyclerViewAdapter(private var items: List<CategoryListViewModel.ListItem>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder? {
+        parent ?: return null
+
+        val itemType = CategoryListViewModel.ListItem.Type.from(viewType)
+        when (itemType) {
+            CategoryListViewModel.ListItem.Type.Caption -> {
+                val binding = CaptionItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                return CaptionItemViewHolder(binding)
+            }
+
+            CategoryListViewModel.ListItem.Type.SubCaption -> {
+                val binding = SubCaptionItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                return SubCaptionItemViewHolder(binding)
+            }
+
+            CategoryListViewModel.ListItem.Type.Category -> {
+                val binding = CategoryItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                return CategoryItemViewHolder(binding)
+            }
+
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
+
+        val item = items[position]
+
+        when {
+            holder is CaptionItemViewHolder && item is CategoryListViewModel.ListItem.CaptionItem -> {
+                @Suppress("MISSING_DEPENDENCY_CLASS")
+                holder.binding.title = item.title
+            }
+
+            holder is SubCaptionItemViewHolder && item is CategoryListViewModel.ListItem.SubCaptionItem -> {
+                @Suppress("MISSING_DEPENDENCY_CLASS")
+                holder.binding.title = item.title
+            }
+
+            holder is CategoryItemViewHolder && item is CategoryListViewModel.ListItem.CategoyItem -> {
+                @Suppress("MISSING_DEPENDENCY_CLASS")
+                holder.binding.title = item.title
+            }
+
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val item = items[position]
+        return item.getType().id
+    }
+
+    override fun getItemCount() = items.count()
+
+    private class CaptionItemViewHolder(val binding: CaptionItemBinding): RecyclerView.ViewHolder(binding.root) { }
+    private class SubCaptionItemViewHolder(val binding: SubCaptionItemBinding): RecyclerView.ViewHolder(binding.root) { }
+    private class CategoryItemViewHolder(val binding: CategoryItemBinding): RecyclerView.ViewHolder(binding.root) { }
+}
